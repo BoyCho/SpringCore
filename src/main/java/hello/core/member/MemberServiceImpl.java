490139da5752abfa1284c61cb2cfe3fb9e5a86df
BoyCho <iijokv@naver.com>
2023-03-07 13:14:31 +0900

    Configuration의 싱글톤 적용 궁금증

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -17,4 +17,9 @@ public class MemberServiceImpl implements MemberService {
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
     }
+
+    // test
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
