bddbf2a1c031e787af558b7f1f6bdae0bdcbe77e
BoyCho <iijokv@naver.com>
2023-03-10 01:38:01 +0900

    빈 스코프  - 싱글톤과 프로토타입 동시 사용

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,11 +1,14 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -37,21 +40,16 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int cnt2 = clientBean2.logic();
-        assertThat(cnt2).isEqualTo(2);
+        assertThat(cnt2).isEqualTo(1);
     }
 
     //@Scope("singleton")
-    static class ClientBean {
+     static class ClientBean {
 
-        private final PrototypeBean prototypeBean;  // 생성 시점에 주입
-
-        //@Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            // 이 때 PrototypeBean 이 조회된 후로는 조회될 일 없음 (조회 = 프로토 타입 스코프 빈의 생성)
-            this.prototypeBean = prototypeBean;
-        }
+        @Autowired private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
