d185e791634bfc8da3ffadb43264a8d7e520b436
BoyCho <iijokv@naver.com>
2023-03-09 16:45:31 +0900

    빈 생명주기 콜백 사용해보기  - @Bean 사용

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -27,16 +24,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
 
     // 서비스 종료 시 호출
     public void disconnect() {
-        System.out.println("close" + url);
+        System.out.println("close: " + url);
     }
 
-    @Override
-    public void destroy() throws Exception {
+    /** 초기화 매서드 **/
+    public void close() {
         disconnect();
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         System.out.println("의존 관계 주입 완료");
         connect();
         call("초기화 연결 메시지");
